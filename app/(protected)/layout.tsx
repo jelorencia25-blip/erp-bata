c88b2fa54776a5a09be92248cdfe32f51aaf4393
Jesslyn <jelorencia25@gmail.com>
2026-02-01 05:32:19 +0700

    force dynamic rendering for all routes

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,3 +1,8 @@
+export const dynamic = 'force-dynamic'
+export const dynamicParams = true
+
+// ... rest of your existing code stays the same
+
 'use client';
 
 import { useEffect, useState } from 'react';
